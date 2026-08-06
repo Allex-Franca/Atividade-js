ebf3450643999d21248e8cfdc1337c7300091dc9
Alex frança <alex_franca150@estudante.sesisenai.org.br>
2026-08-06 15:44:54 -0300

    exerciocio 5 feito

--- script.js
+++ script.js
@@ -49,7 +49,9 @@ document.getElementById("btn4").addEventListener("click", function () {
 // para amarelo
 //
 // Dica: use .style.backgroundColor = "yellow"
-
+document.getElementById("btn5").addEventListener("click",function(){ 
+    document.getElementById("caixa5").style.backgroundColor = "yellow"
+})
 
 
 // ---------- Exercicio 6 ----------
