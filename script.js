3d1d1b787616ba59295c821abf97919989350613
Alex frança <alex_franca150@estudante.sesisenai.org.br>
2026-08-06 15:41:00 -0300

    exerciocio 4 feito

--- script.js
+++ script.js
@@ -37,7 +37,11 @@ document.getElementById("btn3").addEventListener("click",function(){
 // "Novo item" dentro da lista #lista4
 //
 // Dica: use document.createElement() e .appendChild()
-
+document.getElementById("btn4").addEventListener("click", function () {
+    const li = document.createElement("li");
+    li.textContent = "Item 4";
+    document.getElementById("lista4").appendChild(li);
+});
 
 
 // ---------- Exercicio 5 ----------
